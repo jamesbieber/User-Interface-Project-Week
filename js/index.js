44170e3838a5f74e8c94d3c8a5a6047517252db5
jamesbieber <jamesbieber97@gmail.com>
2019-02-21 12:43:15 -0800

    Tabs functionality completed.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-// JS goes here
+class ServiceNav {
+	constructor(service) {
+		this.tabService = document.querySelector(`.tab-content[data-service='${service.dataset.service}']`);
+
+		this.tabService = new TabContent(this.tabService);
+
+		service.addEventListener('click', () => this.serviceClick());
+	}
+
+	serviceClick() {
+		this.tabService.toggleContent();
+	}
+}
+
+class TabContent {
+	constructor(tab) {
+		this.tab = tab;
+	}
+
+	toggleContent() {
+		this.tab.classList.toggle('toggle');
+	}
+}
+
+const services = document.querySelectorAll('.service');
+services.forEach(service => new ServiceNav(service));
