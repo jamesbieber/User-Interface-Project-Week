ef9280038bfe3d4ef13a8e2be4546177c186555a
jamesbieber <jamesbieber97@gmail.com>
2019-02-22 13:19:13 -0800

    Finished services page tabs

--- js/index.js
+++ js/index.js
@@ -1,13 +1,31 @@
+
 class ServiceNav {
 	constructor(service) {
-		this.tabService = document.querySelector(`.tab-content[data-service='${service.dataset.service}']`);
+		this.service = service;
+		this.tabData = this.service.dataset.service;
 
+		// if(this.tabData === 0) {
+		// 	this.items = document.querySelector('.content');
+		// }else{
+		// 	this.items
+		// }
+		this.tabService = document.querySelector(`.tab-content[data-service='${service.dataset.service}']`);
 		this.tabService = new TabContent(this.tabService);
 
 		service.addEventListener('click', () => this.serviceClick());
 	}
 
 	serviceClick() {
+		const tabs = document.querySelectorAll('.service');
+		tabs.forEach(tab => tab.classList.remove('active-service'));
+
+		const content = document.querySelectorAll('.tab-content');
+ 		content.forEach(content => content.style.display = 'none');
+
+ 		const select = document.querySelectorAll('.service');
+ 		Array.from(select).map(content => {})
+
+ 		this.service.classList.add('active-service');
 		this.tabService.toggleContent();
 	}
 }
@@ -18,9 +36,13 @@ class TabContent {
 	}
 
 	toggleContent() {
+		this.tab.style.display = "flex";
 		this.tab.classList.toggle('toggle');
 	}
 }
 
-const services = document.querySelectorAll('.service');
-services.forEach(service => new ServiceNav(service));
+
+
+var services = document.querySelectorAll('.service');
+services = Array.from(services).map(service => new ServiceNav(service));
+services[0].serviceClick();
